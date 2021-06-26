75f23d53ea8a648472017af0d851affeb98b21f2
ashish-greycube <mr.ashish.shah@gmail.com>
2021-06-26 08:43:48 +0200

    initial docs

---
+++ salesman_visit_tracker/salesman_visit_tracker/doctype/customer_visit_plan/customer_visit_plan.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Greycube and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Customer Visit Plan', {
+	// refresh: function(frm) {
+
+	// }
+});
